f597fa0002b348ffac1e7f1f76ba15805f50327e
PureMichael <michael@pureengineering.com>
2017-01-20 14:41:06 -0800

    Create Si1153.c

---
+++ Arduino/Si1153/Si1153.c
@@ -0,0 +1,147 @@
+#include <Arduino.h>
+#include "Si1153.h"
+
+Si1153::Si1153() {
+	
+	//empty constructor
+
+}
+
+/**
+ * Writes data over i2c
+ */
+void Si1153::write_data(uint8_t addr, uint8_t *data, int len){
+
+    Wire.beginTransmission(addr);
+    Wire.write(data, len);
+    Wire.endTransmission();
+  
+}
+
+
+/**
+ * Reads data from a register over i2c
+ */
+int Si1153::read_register(uint8_t addr, uint8_t reg, int bytesOfData){
+  
+    int val = -1;
+  
+    Si1153::write_data(addr, &reg, sizeof(reg));
+    Wire.requestFrom(addr, bytesOfData);
+  
+    if(Wire.available()){
+      val = Wire.read();
+    }
+  
+    return val;
+  
+}
+
+void Si1153::param_set(uint8_t loc, uint8_t val){
+
+	uint8_t packet[2];
+	
+	while(1){
+		int CMMND_CTR = Si1153::read_register(Si1153::DEVICE_ADDRESS,Si1153::RESPONSE0,1);
+
+		packet[0] = Si1153::HOSTIN0;
+		packet[1] = val;
+		Si1153::write_data(Si1153::DEVICE_ADDRESS,packet,2);
+
+		packet[0] = Si1153::COMMAND;
+		packet[1] = loc | (0B10<<6);
+		Si1153::write_data(Si1153::DEVICE_ADDRESS,packet,2);
+
+		int response = Si1153::read_register(Si1153::DEVICE_ADDRESS,Si1153::RESPONSE0,1);
+		if (response > CMMND_CTR){
+			break;
+		}
+	}
+}
+
+
+int Si1153::param_query(uint8_t loc){
+
+	int result = -1;
+	uint8_t packet[2];
+
+	while(1){
+		int CMMND_CTR = Si1153::read_register(Si1153::DEVICE_ADDRESS,Si1153::RESPONSE0,1);
+
+		packet[0] = Si1153::COMMAND;
+		packet[1] = loc | (0B01<<6);
+		Si1153::write_data(Si1153::DEVICE_ADDRESS,packet,2);
+
+		int response = Si1153::read_register(Si1153::DEVICE_ADDRESS,Si1153::RESPONSE0,1);
+
+		if(response>CMMND_CTR){
+			result = Si1153::read_register(Si1153::DEVICE_ADDRESS,Si1153::RESPONSE1,1);
+			break;
+		}
+	}	
+	return result;
+}
+
+/**
+ * Configures a channel at a given index
+ */
+void Si1153::config_channel(uint8_t index, uint8_t *conf){
+
+    int len = sizeof(conf);
+  
+    if(len != 4 || index < 0 || index > 5){
+
+      return;
+        
+    }
+
+    int inc = index * len;
+    
+    param_set(Si1153::ADCCONFIG_0 + inc, conf[0]);
+    param_set(Si1153::ADCSENS_0 + inc, conf[1]);
+    param_set(Si1153::ADCPOST_0 + inc, conf[2]);
+    param_set(Si1153::MEASCONFIG_0 + inc, conf[3]);
+    
+}
+
+/**
+ * Sends command to the command register
+ */
+void Si1153::send_command(uint8_t code){
+
+    uint8_t packet[2];
+
+    while(1){
+  
+        int cmmnd_ctr = Si1153::read_register(Si1153::DEVICE_ADDRESS, Si1153::RESPONSE0, 1);
+      
+        packet[0] = Si1153::COMMAND;
+        packet[1] = code;
+        Si1153::write_data(Si1153::DEVICE_ADDRESS, packet, 2);
+      
+        int r = Si1153::read_register(Si1153::DEVICE_ADDRESS, Si1153::RESPONSE0, 1);
+    
+        if(r > cmmnd_ctr){
+
+            break;
+             
+        }
+        
+    }
+    
+}
+
+int Si1153::bytes_to_int(uint8_t *data, size_t len){
+    int result = 0;
+    int shift = 8 * len;
+
+    for(int i = 0; i < len; i++){
+    
+        shift -= 8;
+        result |= ((data[i] << shift) & (0xFF << shift));
+    
+    }
+
+    return result;
+   
+}
