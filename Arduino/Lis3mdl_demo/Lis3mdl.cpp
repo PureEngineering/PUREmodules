a5e8dbea95c7ee6dccf7a8002727c8c21f9b12b3
PureMichael <michael@pureengineering.com>
2017-01-23 15:22:22 -0800

    Update Lis3mdl.cpp

--- Arduino/Lis3mdl_demo/Lis3mdl.cpp
+++ Arduino/Lis3mdl_demo/Lis3mdl.cpp
@@ -10,10 +10,10 @@ Lis3mdl::Lis3mdl() {
  */
 void Lis3mdl::write_byte(uint8_t addr, uint8_t subAddress, uint8_t data){
 
-    Wire.beginTransmission(addr);       //Send Start Signal and Address over i2c
-    Wire.write(subAddress);             //Subaddress is the register to write to.
-    Wire.write(data);                   //Send Data
-    Wire.endTransmission();             //End I2c communication
+    	Wire.beginTransmission(addr);       //Send Start Signal and Address over i2c
+    	Wire.write(subAddress);             //Subaddress is the register to write to.
+    	Wire.write(data);                   //Send Data
+    	Wire.endTransmission();             //End I2c communication
 }
 
 /**
@@ -21,16 +21,16 @@ void Lis3mdl::write_byte(uint8_t addr, uint8_t subAddress, uint8_t data){
  */
 int Lis3mdl::read_byte(uint8_t addr, uint8_t subAddress){
 
-  	int val = -1;
+	int val = -1;
   	Wire.beginTransmission(addr);         //Send Start Signal and Address over i2c
   	Wire.write(subAddress);               //SubAddress is the register to read from
-    Wire.endTransmission();                
+    	Wire.endTransmission();                
   	Wire.requestFrom(addr,1);             //Request the data from Sensor
     
-    while(Wire.available()==0);
-    val = Wire.read();                    //Read back the data
+    	while(Wire.available()==0);
+    	val = Wire.read();                    //Read back the data
     
-    return val;
+    	return val;
 
 }
 
@@ -39,18 +39,18 @@ int Lis3mdl::read_byte(uint8_t addr, uint8_t subAddress){
  */
 
  int Lis3mdl::read_2bytes(uint8_t addr, uint8_t subAddress){
-    int val = -1;
-    Wire.beginTransmission(addr);        //Send Start Signal and Address over i2c
-    Wire.write(subAddress);              //SubAddress is the register to read from
-    Wire.endTransmission();
-    Wire.requestFrom(addr,2);            //Request two bytes of data from Sensor
-    
-    while(Wire.available()==0);
-    val = Wire.read()<<8;                //First byte is MSB
-    
-    while(Wire.available()==0);
-    val |= Wire.read();                  //Second byte is LSB
-    
-    return val;                          //Return Value
+    	int val = -1;
+    	Wire.beginTransmission(addr);        //Send Start Signal and Address over i2c
+    	Wire.write(subAddress);              //SubAddress is the register to read from
+    	Wire.endTransmission();
+    	Wire.requestFrom(addr,2);            //Request two bytes of data from Sensor
+
+    	while(Wire.available()==0);
+    	val = Wire.read()<<8;                //First byte is MSB
+
+    	while(Wire.available()==0);
+    	val |= Wire.read();                  //Second byte is LSB
+
+    	return val;                          //Return Value
  }
 
