aea53997b9e79282ce35950e83004014ecd5ad3e
PureMichael <michael@pureengineering.com>
2017-02-01 15:48:28 -0800

    Small Bug

--- Arduino/Lis2de/examples/Lis2de_test/Lis2de_test.ino
+++ Arduino/Lis2de/examples/Lis2de_test/Lis2de_test.ino
@@ -8,15 +8,13 @@ void setup() {
   Serial.begin(115200);  
   Wire.begin();  
 
-  //1Hz Sampling rate,  Enable X,Y and Z axes. 
-  //lis2de.write_byte(Lis2de::DEVICE_ADDRESS,Lis2de::CTRL1,0x17);
-  //enable temperature Sensor
-  //lis2de.write_byte(Lis2de::DEVICE_ADDRESS,Lis2de::TEMPCFG,0xC0);
-  //Block data update enable to read out temperature data
-  //lis2de.write_byte(Lis2de::DEVICE_ADDRESS,Lis2de::CTRL4,0x00);
-
+  //Turn on Temp Sensor
   lis2de.settings.TEMP_ENABLE = 3;
+
+  //1Hz Sampling Rate
   lis2de.settings.ODR = 1;
+
+  //Turn on each Axis. 
   lis2de.settings.ZEN = 1;
   lis2de.settings.YEN = 1;
   lis2de.settings.XEN = 1;
