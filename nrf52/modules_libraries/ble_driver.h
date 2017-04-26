97f787c01b55fe80543768431d38fe8bb84c57b0
PureMichael <michael@pureengineering.com>
2017-04-26 18:12:43 +0200

    Moved on/off signals out of here and into supersensor.h

--- nrf52/modules_libraries/ble_driver.h
+++ nrf52/modules_libraries/ble_driver.h
@@ -6,25 +6,6 @@ extern "C" {
 #endif
 
 
-#define LIS2DE_ON_MESSAGE	'1'
-#define LIS2DE_OFF_MESSAGE	'2'
-#define LIS3MDL_ON_MESSAGE	'3'
-#define LIS3MDL_OFF_MESSAGE '4'
-#define BME280_ON_MESSAGE	'5'
-#define BME280_OFF_MESSAGE	'6'
-#define VEML6075_ON_MESSAGE	'7'
-#define VEML6075_OFF_MESSAGE '8'
-#define SI1153_ON_MESSAGE	'9'
-#define SI1153_OFF_MESSAGE	'a'
-#define APDS9250_ON_MESSAGE 'b'
-#define APDS9250_OFF_MESSAGE 'c'
-#define VL53L0_ON_MESSAGE    'd'
-#define VL53L0_OFF_MESSAGE   'e'
-#define P1234701CT_ON_MESSAGE 'f'
-#define P1234701CT_OFF_MESSAGE 'g'
-
-
-
 
 void send_ble_data(ble_nus_t m_nus,uint8_t * data, uint8_t length);
 
