2af94b15fbbdb5511d33b7f2b014c32417cbae5d
PureMichael <michael@pureengineering.com>
2017-05-14 20:17:38 -0400

    Cleanup for others to use DFU functionality.

--- nrf52/modules_examples/nrf52_secure_bootloader/pca10040/armgcc/program.bat
+++ nrf52/modules_examples/nrf52_secure_bootloader/pca10040/armgcc/program.bat
@@ -1,2 +1,2 @@
-mergehex -m C:\Users\michael\Desktop\nRF5_SDK_12.2.0_f012efa\components\softdevice\s132\hex\s132_nrf52_3.0.0_softdevice.hex ./_build/nrf52832_xxaa_s132.hex -o nrf52832_s132.hex
+mergehex -m ..\..\..\..\..\nRF5_SDK_12.2.0_f012efa\components\softdevice\s132\hex\s132_nrf52_3.0.0_softdevice.hex ./_build/nrf52832_xxaa_s132.hex -o nrf52832_s132.hex
 nrfjprog --chiperase --program nrf52832_s132.hex -f NRF52 -r
