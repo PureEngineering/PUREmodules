a8d2e0cfee0ae6fa5ed4f85be9218c7efaca1890
PureMichael <michael@pureengineering.com>
2017-05-14 20:03:33 -0400

    Added pureModules dfu package

--- nrf52/modules_examples/nrf52_supersensor_template_ble/pca10040/s132/armgcc/buildit.bat
+++ nrf52/modules_examples/nrf52_supersensor_template_ble/pca10040/s132/armgcc/buildit.bat
@@ -1,3 +1,3 @@
 make 
-nrfutil pkg generate --hw-version 52 --sd-req 0x8C --application-version 4 --application ./_build/nrf52832_xxaa.hex --key-file ../../../../nrf52_secure_bootloader/private.pem puremodules_uart_dfu.zip
+nrfutil pkg generate --hw-version 52 --sd-req 0x8C --application-version 4 --application ./_build/nrf52832_xxaa_s132.hex --key-file ../../../../private.pem puremodules_uart_dfu.zip
 pause
