445a8fad6a38c9d1b1a509e2b424dd6d985a7a3a
PureMichael <michael@pureengineering.com>
2017-07-05 17:01:59 +0200

    Completed tmp007 driver (untested)

--- nrf52/modules_libraries/tmp007.h
+++ nrf52/modules_libraries/tmp007.h
@@ -11,13 +11,26 @@
 
 // uncomment for debugging!
 #define TMP007_DEBUG 1
-
-#define TMP007_VOBJ       0x00
-#define TMP007_TDIE       0x01
-#define TMP007_CONFIG     0x02
-#define TMP007_TOBJ       0x03
-#define TMP007_STATUS     0x04
-#define TMP007_STATMASK   0x05
+                                     //DEFAULTS
+#define TMP007_VOBJ       0x00       //0x0000 
+#define TMP007_TDIE       0x01       //0x0000
+#define TMP007_CONFIG     0x02       //0x1440
+#define TMP007_TOBJ       0x03       //0x0000
+#define TMP007_STATUS     0x04       //0x0000
+#define TMP007_STATMASK   0x05       //0x0000
+#define TMP007_TOBJ_HIGHLIM_REG 0x06 //0x7FC0
+#define TMP007_TOBJ_LOWLIM_REG  0x07 //0X8000
+#define TMP007_TDIE_HIGHLIM_REG 0X08 //0x7FC0
+#define TMP007_TDIE_LOWLIM_REG  0X09 //0x8000
+#define TMP007_S0               0X0A //0x260E
+#define TMP007_A1               0x0B //0x0106
+#define TMP007_A2               0x0C //0xFF9B
+#define TMP007_B0               0x0D //0xFF3A
+#define TMP007_B1               0x0E //0xFF71
+#define TMP007_B2               0x0F //0x0553
+#define TMP007_C2               0x10 //0x0000
+#define TMP007_TC0              0x11 //0x0034
+#define TMP007_TC1              0x12 //0x0000
 
 #define TMP007_CFG_RESET    0x8000
 #define TMP007_CFG_MODEON   0x1000
@@ -34,7 +47,8 @@
 #define TMP007_STAT_CRTEN   0x4000
 
 #define TMP007_DEVICE_ADDR 0x40
-#define TMP007_DEVID 0x1F
+#define TMP007_MANUFACT_ID 0x1E     //0x5449
+#define TMP007_DEVID 0x1F           //0x0078
 
 
 
@@ -42,13 +56,13 @@
   //uint16_t samplerate = TMP007_CFG_16SAMPLE;
 
   uint8_t tmp007_begin(nrf_drv_twi_t twi_master,uint16_t samplerate);  // by default go highres
-  bool tmp007_pass(nrf_drv_twi_t twi_master);
+  bool    tmp007_pass(nrf_drv_twi_t twi_master);
   uint8_t tmp007_whoami(nrf_drv_twi_t twi_master);
 
   int16_t tmp007_readRawDieTemperature(nrf_drv_twi_t twi_master);
   int16_t tmp007_readRawVoltage(nrf_drv_twi_t twi_master);
-  double tmp007_readObjTempC(nrf_drv_twi_t twi_master);
-  double tmp007_readDieTempC(nrf_drv_twi_t twi_master);
+  double  tmp007_readObjTempC(nrf_drv_twi_t twi_master);
+  double  tmp007_readDieTempC(nrf_drv_twi_t twi_master);
 
 
 #endif
