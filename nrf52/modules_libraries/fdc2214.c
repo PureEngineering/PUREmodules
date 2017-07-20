c3b0fda1023efc1cfeae692b65e2464694eef0b7
PureMichael <michael@pureengineering.com>
2017-07-20 18:45:37 +0200

    Start of capacitive sensor code

---
+++ nrf52/modules_libraries/fdc2214.c
@@ -0,0 +1,16 @@
+#include "fdc2214.h"
+
+#define NRF_LOG_MODULE_NAME "APP"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_drv_twi.h"
+#include "bsp.h"
+#include "i2c_driver.h"
+#include "ble_driver.h"
+#include "ble_nus.h"
+#include "nrf_delay.h"
+
+
+
+
+
---
+++ nrf52/modules_libraries/fdc2214.h
@@ -0,0 +1,59 @@
+#ifndef ADS1114_H
+#define ADS1114_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "nrf_drv_twi.h"
+#define NRF_LOG_MODULE_NAME "APP"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "ble_nus.h"
+
+
+#define FDC2214_DATA_MSW_CH0 0x00
+#define FDC2214_DATA_LSW_CH0 0x01
+#define FDC2214_DATA_MSW_CH1 0x02
+#define FDC2214_DATA_LSW_CH1 0x03
+#define FDC2214_DATA_MSW_CH2 0x04
+#define FDC2214_DATA_LSW_CH2 0x05
+#define FDC2214_DATA_MSW_CH3 0x06
+#define FDC2214_DATA_LSW_CH3 0x07
+#define FDC2214_RCOUNT_CH0 0x08
+#define FDC2214_RCOUNT_CH1 0x09
+#define FDC2214_RCOUNT_CH2 0x0a
+#define FDC2214_RCOUNT_CH3 0x0b
+#define FDC2214_OFFSET_CH0 0x0c
+#define FDC2214_OFFSET_CH1 0x0d
+#define FDC2214_OFFSET_CH2 0x0e
+#define FDC2214_OFFSET_CH3 0x0f
+#define FDC2214_SETTLECOUNT_CH0 0x10
+#define FDC2214_SETTLECOUNT_CH1 0x11
+#define FDC2214_SETTLECOUNT_CH2 0x12
+#define FDC2214_SETTLECOUNT_CH3 0x13
+#define FDC2214_CLOCK_DIVIDERS_CH0 0x14
+#define FDC2214_CLOCK_DIVIDERS_CH1 0x15
+#define FDC2214_CLOCK_DIVIDERS_CH2 0x16
+#define FDC2214_CLOCK_DIVIDERS_CH3 0x17
+#define FDC2214_DRIVE_CURRENT_CH0 0x1e
+#define FDC2214_DRIVE_CURRENT_CH1 0x1f
+#define FDC2214_DRIVE_CURRENT_CH2 0x20
+#define FDC2214_DRIVE_CURRENT_CH3 0x21
+#define FDC2214_ERROR_CONFIG 0x19
+#define FDC2214_MUX_CONFIG 0x1b
+#define FDC2214_RESET_DEV 0x1c
+#define FDC2214_CONFIG 0x1a
+#define FDC2214_STATUS 0x18
+#define FDC2214_FIN 0xff
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ADS1114
+
