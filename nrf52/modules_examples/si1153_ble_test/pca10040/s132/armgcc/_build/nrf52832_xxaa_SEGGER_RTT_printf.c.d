e998f543a15233df979ca01c05dd193a8c5780f6
Wuyuan <wuyuan@pureengineering.com>
2017-07-19 18:14:14 -0700

    si1153 can receive data from phone using BLE

---
+++ nrf52/modules_examples/si1153_ble_test/pca10040/s132/armgcc/_build/nrf52832_xxaa_SEGGER_RTT_printf.c.d
@@ -0,0 +1,2 @@
+_build/nrf52832_xxaa_SEGGER_RTT_printf.c.o: \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/external/segger_rtt/SEGGER_RTT_printf.c
---
+++ nrf52/modules_examples/si1153_ble_test/pca10040/s132/armgcc/_build/nrf52832_xxaa_system_nrf52.c.d
@@ -0,0 +1,65 @@
+_build/nrf52832_xxaa_system_nrf52.c.o: \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/system_nrf52.c \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\lib\gcc\arm-none-eabi\5.3.1\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\machine\_default_types.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\sys\features.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\_newlib_version.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\sys\_intsup.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\sys\_stdint.h \
+ c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\lib\gcc\arm-none-eabi\5.3.1\include\stdbool.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf52.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/system_nrf52.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf52_bitfields.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf51_to_nrf52.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf52_name_change.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/device/compiler_abstraction.h \
+ ../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/system_nrf52.h
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\lib\gcc\arm-none-eabi\5.3.1\include\stdint.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\stdint.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\machine\_default_types.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\sys\features.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\_newlib_version.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\sys\_intsup.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\arm-none-eabi\include\sys\_stdint.h:
+
+c:\program\ files\ (x86)\gnu\ tools\ arm\ embedded\5.3\ 2016q1\lib\gcc\arm-none-eabi\5.3.1\include\stdbool.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf52.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/system_nrf52.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf52_bitfields.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf51_to_nrf52.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/device/nrf52_name_change.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/device/compiler_abstraction.h:
+
+../../../../../nRF5_SDK_12.2.0_f012efa/components/toolchain/system_nrf52.h:
