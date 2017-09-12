f0a32bc633299c1729ee50d13b629cdbad728f13
Sashi Ono <info@pureengineering.com>
2017-09-11 19:40:43 -0700

    fixed PN

--- nrf52/modules_libraries/lsm6dsm.c
+++ nrf52/modules_libraries/lsm6dsm.c
@@ -10,25 +10,30 @@
 #include "nrf_drv_twi.h"
 
 
-uint8_t defaut_lsm6ds3_address = LSM6DS3_DEVICE_ADDRESS;
+uint8_t defaut_lsm6dsm_address = LSM6DSM_DEVICE_ADDRESS;
 
-uint8_t lsm6ds3_whoami(nrf_drv_twi_t twi_master)
+uint8_t lsm6dsm_whoami(nrf_drv_twi_t twi_master)
 {
-	uint8_t who_am_i = read_byte(twi_master,defaut_lsm6ds3_address,LSM6DS3_ACC_GYRO_WHO_AM_I_REG);
+	uint8_t who_am_i = read_byte(twi_master,defaut_lsm6dsm_address,LSM6DSM_ACC_GYRO_WHO_AM_I_REG);
 	return who_am_i;
 }
 
-uint8_t lsm6ds3_init(nrf_drv_twi_t twi_master)
+uint8_t lsm6dsm_gyro_read()
+{
+}
+
+
+uint8_t lsm6dsm_init(nrf_drv_twi_t twi_master)
 {
 
-	uint8_t who_am_i =  lsm6ds3_whoami(twi_master);
+	uint8_t who_am_i =  lsm6dsm_whoami(twi_master);
 	if(who_am_i != 0x6a)
 	{
-		NRF_LOG_RAW_INFO("ERROR:lsm6ds3 who_am_i 0x%x != 0x6a \r\n", who_am_i);
+		NRF_LOG_RAW_INFO("ERROR:lsm6dsm who_am_i 0x%x != 0x6a \r\n", who_am_i);
 	}
 	else
 	{
-		NRF_LOG_RAW_INFO("lsm6ds3 who_am_i 0x%x == 0x6a \r\n", who_am_i);
+		NRF_LOG_RAW_INFO("lsm6dsm who_am_i 0x%x == 0x6a \r\n", who_am_i);
 	}
 
 	return who_am_i;
