937796422a22ec6847359fc7114d10d219859135
Bradley Wolf <bradleywolf@Bradleys-MacBook-Air-2.local>
2018-01-17 17:09:32 -0500

    Added back acks

--- nrf52/modules_libraries/DW1000.h
+++ nrf52/modules_libraries/DW1000.h
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
+ * Decawave DW1000 library for arduino.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file DW1000.h
+ * Arduino driver library (header file) for the Decawave DW1000 UWB transceiver IC.
+ * 
+ * @todo
+ * - impl: later:
+ * - TXBOFFS in TX_FCTRL for offset buffer transmit
+ * - TR in TX_FCTRL for flagging for ranging messages
+ * - CANSFCS in SYS_CTRL to cancel frame check suppression
+ * - HSRBP in SYS_CTRL to determine in double buffered mode from which buffer to read
+ */
+
+/*
+*   All arduino dependencies and code changes for use with nrf52
+*
+*/
+
+
 #ifndef DW1000_H
 #define DW1000_H
 
--- nrf52/modules_libraries/DW1000Time.h
+++ nrf52/modules_libraries/DW1000Time.h
@@ -1,3 +1,34 @@
+/**
+ * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
+ * Copyright (c) 2016 by Ludwig Grill (www.rotzbua.de); refactored class
+ * Decawave DW1000 library for arduino.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @file DW1000Time.h
+ * Arduino driver library timestamp wrapper (header file) for the Decawave 
+ * DW1000 UWB transceiver IC.
+ * 
+ * @TODO
+ * - avoid/remove floating operations, expensive on most microprocessors
+ * 
+ * @note
+ * comments in cpp file, makes .h smaller and gives a better overview about
+ * available methods and variables.
+ */
+
+
+
 #ifndef _DW1000Time_H_INCLUDED
 #define _DW1000Time_H_INCLUDED
 
