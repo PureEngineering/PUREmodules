7cb8447f72f171831abc328532a3b5015a4fff14
michael <michael@pureengineering.com>
2018-04-10 07:50:18 -0800

    Removed projects from modules

--- nrf52/modules_examples/dwm1000_test/pca10040/s132/armgcc/program.bat
+++
@@ -1,2 +0,0 @@
-mergehex -m ..\..\..\..\..\nRF5_SDK_12.2.0_f012efa\components\softdevice\s132\hex\s132_nrf52_3.0.0_softdevice.hex ./_build/nrf52832_xxaa.hex -o nrf52832_s132.hex
-nrfjprog --chiperase --program nrf52832_s132.hex -f NRF52 -r
