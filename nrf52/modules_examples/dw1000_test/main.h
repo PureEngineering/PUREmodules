0b7a2683180c21a9469c5ae747ac01ce2e55f0b7
michael <michael@pureengineering.com>
2018-04-09 16:25:28 -0800

    Removed projects that were nonworking attempts

--- nrf52/modules_examples/dw1000_test/main.h
+++
@@ -1,7 +0,0 @@
-#include "nrf_drv_spi.h"
-
-
- #define SPI_INSTANCE  2 //< SPI instance index. 
-
- static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);  //< SPI instance. 
- static volatile bool spi_xfer_done;  //< Flag used to indicate that SPI instance completed the transfer. 
